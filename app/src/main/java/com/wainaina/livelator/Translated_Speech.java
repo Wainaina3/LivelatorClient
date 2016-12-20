962f437e1f10fdc826b4c3384e2093380d99dd8c
Wainaina <dwainaina3@gmail.com>
2016-12-20 12:25:36 +0300

    Initial commit for livelator android

---
+++ app/src/main/java/com/wainaina/livelator/Translated_Speech.java
@@ -0,0 +1,153 @@
+package com.wainaina.livelator;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Translated_Speech.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Translated_Speech#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Translated_Speech extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public Translated_Speech() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment Translated_Speech.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Translated_Speech newInstance() {
+       // Translated_Speech fragment = new Translated_Speech();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+        return new Translated_Speech();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_translated__speech, container, false);
+        listen_to_fragment_events(view);
+
+        return view;
+    }
+
+    //create event listeners to fragment views
+    private void listen_to_fragment_events(View view) {
+        play_pause_button(view);
+
+    }
+
+    private void play_pause_button(View view){
+        ImageView play_pause_s;
+        play_pause_s=(ImageView)view.findViewById(R.id.play_pause_s);
+        play_pause_s.setTag("play");
+
+        play_pause_s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                change_play_pause(view);
+            }
+        });
+    }
+
+    private void change_play_pause(View view) {
+
+        //check the view and change accordingly
+        ImageView play_pause = (ImageView)view;
+        Integer play_pause_id = view.getId();
+
+        if(play_pause_id==R.id.play_pause_s){
+            if(play_pause.getTag().equals("play")){
+                play_pause.setImageResource(R.mipmap.pause_circle);
+                play_pause.setTag("pause");
+
+            }
+            else{
+                play_pause.setImageResource(R.mipmap.play_circle);
+                play_pause.setTag("play");
+            }
+
+        }
+    }
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(View view) {
+        if (mListener != null) {
+            mListener.translated_Speech_interface_callback();
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void translated_Speech_interface_callback();
+    }
+}
---
+++ app/src/main/java/com/wainaina/livelator/home.java
@@ -0,0 +1,140 @@
+package com.wainaina.livelator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+public class home extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,Translate_Text.OnFragmentInteractionListener,
+        Translated_Speech.OnFragmentInteractionListener,Profile.OnFragmentInteractionListener {
+    //Define objects
+    protected DrawerLayout drawer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if(findViewById(R.id.fragment_container)!=null){
+            if(savedInstanceState!=null){
+                return;
+            }
+
+            //initialize speech fragment
+            Translated_Speech translated_speech_frag = new Translated_Speech();
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, translated_speech_frag).commit();
+
+            getSupportActionBar().setTitle(R.string.live_translation_title);
+
+        }
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+
+        if (id == R.id.nav_speech) {
+            Translated_Speech translated_speech_frag = new Translated_Speech();
+            fragmentTransaction.replace(R.id.fragment_container,translated_speech_frag).commit();
+            getSupportActionBar().setTitle(R.string.live_translation_title);
+
+
+        } else if (id == R.id.nav_subtitles) {
+            Translate_Text translate_text_frag = new Translate_Text();
+            fragmentTransaction.replace(R.id.fragment_container,translate_text_frag).commit();
+            getSupportActionBar().setTitle(R.string.translated_text_title);
+
+        } else if (id == R.id.nav_profile) {
+            Profile profile_frag = new Profile();
+            fragmentTransaction.replace(R.id.fragment_container,profile_frag).commit();
+            getSupportActionBar().setTitle(R.string.profile_title);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //implement private OnFragmentInteractionListener mListener
+    public void translated_Speech_interface_callback() {
+        //do some stuff
+    }
+
+    //implement private OnFragmentInteractionListener mListener
+    public void translated_text_interface_callback() {
+        //do some stuff
+    }
+
+    //implement private OnFragmentInteractionListener mListener
+    public void profile_interface_callback() {
+        //do some stuff
+    }
+
+}
